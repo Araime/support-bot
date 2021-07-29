8b00726cc9ef97275406677867dab62c7d72aa54
Araime <alvonsa09121985@gmail.com>
2021-07-29 21:49:08 +0400

    1. Add a handler to handle all errors. This frees the programmer from the need to write try/except only for logging errors. 2. Add ChatAction.Typing 3. Delete try/except block

--- tg_bot.py
+++ tg_bot.py
@@ -1,7 +1,9 @@
 import logging
 import os
+import time
 
 from dotenv import load_dotenv
+from telegram.chataction import ChatAction
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 from dialog import detect_intent_texts
@@ -12,23 +14,28 @@ logger = logging.getLogger('tg_bot')
 
 def start(update, context: CallbackContext):
     """Sending a message when the command /start is issued."""
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    time.sleep(1)
     update.message.reply_text('Добрый день! Бот поддержки рад приветствовать вас!')
+
+
+def error_callback(update, error):
+    """Handling all errors."""
+    logger.exception('Update "%s" caused error "%s"', update, error)
 
 
 def reply_to_message(update, context: CallbackContext):
     """Responding to user messages using dialogflow."""
     project_id = os.getenv('PROJECT_ID')
 
-    # noinspection PyBroadException
-    try:
-        answer = detect_intent_texts(
-            project_id,
-            update.message.chat_id,
-            update.message.text,
-            'ru-RU')
-        update.message.reply_text(answer['answer_text'])
-    except Exception:
-        logger.exception('tg_bot поймал ошибку при сообщения')
+    answer = detect_intent_texts(
+        project_id,
+        update.message.chat_id,
+        update.message.text,
+        'ru-RU')
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    time.sleep(1)
+    update.message.reply_text(answer['answer_text'])
 
 
 def main():
@@ -36,17 +43,14 @@ def main():
 
     configure_handler(logger, os.getenv('TG_SERVICE_BOT'), os.getenv('TG_CHAT_ID'))
 
-    # noinspection PyBroadException
-    try:
-        logger.info('tg_bot запущен!')
-        updater = Updater(os.getenv('TG_DIALOG_BOT'))
-        dispatcher = updater.dispatcher
-        dispatcher.add_handler(CommandHandler('start', start))
-        dispatcher.add_handler(MessageHandler(Filters.text, reply_to_message))
-        updater.start_polling()
-        updater.idle()
-    except Exception:
-        logger.exception('tg_bot поймал ошибку: ')
+    logger.info('tg_bot запущен!')
+    updater = Updater(os.getenv('TG_DIALOG_BOT'))
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.text, reply_to_message))
+    dispatcher.add_error_handler(error_callback)
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
